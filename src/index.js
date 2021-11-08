72c745593373941f2ed857b6425117598d859492
Gene4kaS <jannasavchenko@gmail.com>
2021-11-08 22:20:15 +0200

    scroll

--- src/index.js
+++ src/index.js
@@ -1,79 +1,98 @@
 import './css/common.css';
 import apiServise from './js/apiService';
 import cardsGallery from './templates/gallery-card.hbs';
+import * as PNotify from "@pnotify/core";
+import * as PNotifyMobile from "@pnotify/mobile";
+import "@pnotify/core/dist/BrightTheme.css";
+
+const myStack = new PNotify.Stack({
+    dir1: "up",
+  });
+  
+function onError() {
+    return PNotify.notice({
+            text: "Please, enter a correct request!",
+            stack: myStack,
+            modules: new Map([...PNotify.defaultModules, [PNotifyMobile, {}]]),
+          });
+  }
 
 const refs = {
     searchForm: document.querySelector('#search-form'),
     gallery: document.querySelector('.gallery'),
     loadMoreBtn: document.querySelector('button[data-action="load-more"]'),
+    modal: document.getElementById('modal-js'),
+    largeImg: document.querySelector('.image-large'),
 };    
 
 refs.searchForm.addEventListener('submit', onSearch);
-refs.loadMoreBtn.addEventListener('click', loadMoreBtnHandler);
+refs.gallery.addEventListener('click', showLargeImg);
+refs.modal.addEventListener('click', () => {
+  refs.modal.style.display = 'none';
+  setAttrImg('', '', 'hidden');
+});
+// refs.loadMoreBtn.addEventListener('click', loadMore);
+
 
 function clearListItems() {
     refs.gallery.innerHTML = '';
+    myStack.close(true);
   }
 
 function onSearch(element) {
   element.preventDefault();
-  const form = element.currentTarget;
-  const input = form.elements.query;
+  apiServise.searchQuerry = element.currentTarget.elements.query.value;
+
   clearListItems();
-
   apiServise.resetPage();
-  apiServise.searchQuerry = input.value;
+  fetchGallery();
 
-  if (!apiServise.searchQuerry.trim() || apiServise.searchQuerry === '') {
+  if (!apiServise.searchQuerry.trim()) {
     return alert('ENTRY ERROR');
   }
-
-  apiServise.fetcArticles().then(hits => {
-    const markup = buildListItemsTemplate(hits);
-    useListItems(markup);
-  });
-  input.value = '';
-
 }
 
-function useListItems(items) {
-    refs.gallery.insertAdjacentHTML('beforeend', items);
-   scrollInto(); 
-  }
-
-  function buildListItemsTemplate(items) {
-    return cardsGallery(items);
-  }
-
-function loadMoreBtnHandler() {
-    apiServise.fetcArticles()
-    .then(hits => {
-    const markup = buildListItemsTemplate(hits);
-    useListItems(markup);
+function fetchGallery() {
+    apiServise.fetchArticles().then(hits => {
+        refs.gallery.insertAdjacentHTML('beforeend', cardsGallery(hits));
   })
-  .catch(hits => alert(`Error: ${hits}`));
+  .catch(console.log(onError()));
 }
 
-function scrollInto() {
+function scroll() {
     refs.loadMoreBtn.scrollIntoView({
       behavior: 'smooth',
       block: 'end',
     })
 }
 
+document.addEventListener('scroll', () => {
+    const documentRect = document.documentElement.getBoundingClientRect();
+    if (documentRect.bottom < document.documentElement.clientHeight + 200) {
+        loadMore();
+    }
+})
 
-// window.addEventListener('scroll', () => {
-//     const documentRect = document.documentElement.getBoundingClientRect();
-//     if (documentRect.bottom < document.documentElement.clientHeight + 200) {
-//         apiServise.page++;
-//         cardsGallery(items);
-//     }
-    
-// })
+function loadMore() {
+    fetchGallery()
+    scroll();
+  };
 
 // [].forEach.call(document.getElementsByClassName('number'), e => 
 // e.textContent = e.textContent.replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 '));
 
-
-// key: 563492ad6f91700001000001346204d6cc5d4973a3d0c717f03cff8
-// https://api.pexels.com/v1/search?query=people
+function showLargeImg(element) {
+    if (!element.target.dataset.src) return;
+    refs.modal.style.display = 'flex';
+    setAttrImg(element.target.dataset.src, element.target.alt, 'show');
+  }
+  
+  function setAttrImg(src, alt, status) {
+    refs.largeImg.src = src;
+    refs.largeImg.alt = alt;
+    if (status === 'show') 
+         refs.largeImg.classList.add('js-show');
+    else refs.largeImg.classList.remove('js-show');
+    return;
+  }
+  
--- src/js/apiService.js
+++ src/js/apiService.js
@@ -1,11 +1,11 @@
 
 const BASE_URL = 'https://pixabay.com/api/';
+const API_KEY = '24210737-3b0bc435d65d70e1c06573fda';
 
 export default {
     page: 1,
     query: '',
-    async fetcArticles(query) {
-      const API_KEY = '24210737-3b0bc435d65d70e1c06573fda';
+    async fetchArticles(query) {
       const url = `?image_type=photo&orientation=horizontal&q=${this.query}&page=${this.page}&per_page=12&key=${API_KEY}`;
       const res = await fetch(BASE_URL + url);
       const parseRes = await res.json();
